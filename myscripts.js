e3bec3655798e61d5b3561f63d1528e94cbc8cd3
Remy Guts <remyguts@gmail.com>
2020-12-15 14:21:01 -0800

    build first rectangle

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,12 @@
 function draw() {
   var canvas = document.getElementById("tutorial");
   if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
+      var ctx = canvas.getContext("2d");
+      
+      ctx.fillStyle = 'rgb(200, 0, 0)';
+      ctx.fillRect(10, 10, 50, 50);
   }
 }
 
-function draw() {
-  var canvas = document.getElementById("canvas");
+
 }
