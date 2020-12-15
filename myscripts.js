c41bebb4b117c05bc0b655c2db0b8f72516b0279
Remy Guts <remyguts@gmail.com>
2020-12-15 14:28:59 -0800

    build second rectangle part 2

--- myscripts.js
+++ myscripts.js
@@ -6,7 +6,7 @@ function draw() {
     ctx.fillStyle = "rgb(200, 0, 0)";
     ctx.fillRect(10, 10, 50, 50);
 
-    ctx.fillStyle = "rgba(0,0,200,0.5)";
+    ctx.fillStyle = "rgba(8,8,8,0.5)";
     ctx.fillRect(30, 30, 50, 50);
   }
 }
